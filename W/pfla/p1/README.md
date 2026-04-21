b43430251b8ecaaeeb120761affcf812f035d0b7
m0x0m0x <123001669+m0x0m0x@users.noreply.github.com>
2026-04-21 04:37:31 +0000

    🫦

---
+++ W/pfla/p1/README.md
@@ -0,0 +1,15 @@
+<h3 align="center"> <i> p1 - 1st version of python flask </i> </h3>
+
+1. [What](#what)
+2. [Dirz](#dirz)
+
+# What
+
+1. First version of python flasks study
+
+# Dirz
+
+|           Dir           |       What        |
+| :---------------------: | :---------------: |
+| [`runs.sh`](./runs.sh/) | Logs from runz.sh |
+|    [`logz`](./logz/)    |  Logs from ru.sh  |
---
+++ W/pfla/p1/runz.sh
@@ -0,0 +1,41 @@
+#!/usr/bin/env bash
+
+# -------------------------
+#  UV Run
+# -------------------------
+
+# Error Handling
+set -euo pipefail
+
+# Commands
+
+hea1() {
+	echo -e ""
+	echo -e ""
+	echo -e "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
+	echo -e "$1$"
+	echo -e "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
+}
+
+# UV Setup
+
+uv_s1() {
+	hea1 "UV Setup 1 with FastAPI"
+
+	# UC Commands
+	CO1="uv run main.py"
+
+	## RUN Above Commands
+	echo -e "--- Executing ${CO1} ---"
+	eval "$CO1"
+	echo -e ""
+	echo -e ""
+	echo -e ""
+
+}
+
+# --- Execution ---
+panty() {
+	uv_s1 2>&1 | tee -a logz/rushlogs.txt
+}
+panty
