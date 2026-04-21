0f559fe557c56e795b9715d4dd5a895bcebd36e1
m0x0m0x <123001669+m0x0m0x@users.noreply.github.com>
2026-04-21 10:14:13 +0000

    🫦

--- W/pfla/p2/runz.sh
+++ W/pfla/p2/runz.sh
@@ -7,6 +7,17 @@
 # Error Handling
 set -euo pipefail
 
+# Colors
+BBLACK='\033[1;90m'
+BRED='\033[1;91m'
+BGREEN='\033[1;92m'
+BYELLOW='\033[1;93m'
+BBLUE='\033[1;94m'
+BMAGENTA='\033[1;95m'
+BCYAN='\033[1;96m'
+BWHITE='\033[1;97m'
+RESET='\033[0m'
+
 # Commands
 
 hea1() {
@@ -34,8 +45,41 @@ uv_s1() {
 
 }
 
+# Sending Curl requests to the Flask Endpoint for testing
+sendCurl() {
+
+	# Endpoint
+	EP="https://fluffy-telegram-97679qp95pvf4xg-5000.app.github.dev/"
+
+	declare -a CMD=(
+
+		#0 -  Install Render Cli -Docs - https://render.com/docs/cli
+		"curl https://fluffy-telegram-97679qp95pvf4xg-5000.app.github.dev/"
+
+		#1 -  Testing endpoint
+		"curl https://fluffy-telegram-97679qp95pvf4xg-5000.app.github.dev/greet/panty"
+
+		#2 - Testing the route which got deployed on vercel shows index
+		"curl https://fluffy-telegram-97679qp95pvf4xg-5000.app.github.dev/index"
+
+	)
+
+	CMDEXEC="${CMD[2]}"
+	echo -e "${BBLUE} · · ────── ꒰ঌ·✦·໒꒱ ────── · ·"
+	echo -e "${BBLUE} · · ────── Sending Curl Requests ────── · ·"
+	echo -e "${BBLUE} · · ────── ꒰ঌ·✦·໒꒱ ────── · ·"
+	date
+	echo -e "Executing:${BMAGENTA}${CMDEXEC}${RESET}"
+	eval "${CMDEXEC}"
+	echo -e "${BGREEN}Done!"
+	echo -e "───── ⋆⋅☆⋅⋆ ─────${RESET}"
+	echo -e "───── ⋆⋅☆⋅⋆ ─────${RESET}"
+	echo -e "───── ⋆⋅☆⋅⋆ ─────${RESET}"
+}
+
 # --- Execution ---
 panty() {
-	uv_s1 2>&1 | tee -a logz/rushlogs.txt
+	# uv_s1 2>&1 | tee -a logz/runz.sh.txt
+	sendCurl 2>&1 | tee -a logz/runz.sh.txt
 }
 panty
