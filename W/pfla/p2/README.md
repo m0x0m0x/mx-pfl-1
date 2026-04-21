e2014e9df16ba275bca3fbd5a2bddc0dcf436fd8
m0x0m0x <123001669+m0x0m0x@users.noreply.github.com>
2026-04-21 06:20:21 +0000

    🫦

---
+++ W/pfla/p2/README.md
@@ -0,0 +1,20 @@
+<h3 align="center"> <i> p1 - 1st version of python flask </i> </h3>
+
+1. [What](#what)
+2. [Dirz](#dirz)
+3. [App Summary](#app-summary)
+
+# What
+
+1. First version of python flasks study
+
+# Dirz
+
+|           Dir           |       What        |
+| :---------------------: | :---------------: |
+| [`runs.sh`](./runs.sh/) | Logs from runz.sh |
+|    [`logz`](./logz/)    |  Logs from ru.sh  |
+
+# App Summary
+
+1. Testing more endpoints to resemble a webapp
--- W/pfla/p2/app.py
+++ W/pfla/p2/app.py
@@ -11,9 +11,29 @@ app = Flask(__name__)
 # --- Routes ---
 
 
+# Index page
 @app.route('/')
-def home():
+def index():
     return "<h1> Booty Dance Smill0 </h1>"
+
+
+@app.route('/pusy')
+def hello():
+    return "<h2> Enjoy strong pussy stink</h2>"
+
+# Handling Variables in URL - Url procesors
+
+
+@app.route('/greet/<name>')
+def greet(name):
+    return f"<code></h1> Sup, {name}, My Niggaz </h1></code>"
+
+# Route for adding two number
+
+
+@app.route('/add/<int:num1>/<int:num2>')
+def add(num1, num2):
+    return f"<h2>{num1} + {num2} = {num1 + num2}</h2>"
 
 
 # --- Run App ---
