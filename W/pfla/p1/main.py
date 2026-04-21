0a6662f337957f6005b4139676bbe3f2fc8aa881
m0x0m0x <123001669+m0x0m0x@users.noreply.github.com>
2026-04-21 04:53:28 +0000

    🫦

--- W/pfla/p1/main.py
+++ W/pfla/p1/main.py
@@ -7,11 +7,10 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return "Boooty Dancer"
+    return "<h1>Boooty Dancer Dundo</h1>"
 
 
 if __name__ == '__main__':
     app.run(
         debug=True,
-        host='0.0.0.0',
     )
