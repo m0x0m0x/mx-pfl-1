3a2743e93115673d0f568e57d678919130038b5a
m0x0m0x <123001669+m0x0m0x@users.noreply.github.com>
2026-04-27 10:06:00 +0000

    🤡 p3 work

---
+++ W/pfla/p3/sc/r1.sh
@@ -0,0 +1,52 @@
+#!/usr/bin/env bash
+
+# -------------------------
+#  UV Run
+# -------------------------
+
+# Error Handling
+set -euo pipefail
+
+# Colors
+BBLACK='\033[1;90m'
+BRED='\033[1;91m'
+BGREEN='\033[1;92m'
+BYELLOW='\033[1;93m'
+BBLUE='\033[1;94m'
+BMAGENTA='\033[1;95m'
+BCYAN='\033[1;96m'
+BWHITE='\033[1;97m'
+RESET='\033[0m'
+
+# Commands
+
+hea1() {
+	echo -e ""
+	echo -e ""
+	echo -e "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
+	echo -e "$1$"
+	echo -e "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
+}
+
+# UV Setup
+
+uv_s1() {
+	hea1 "UV Setup 1 with FastAPI"
+
+	# UC Commands
+	CO1="uv run ./pyz/app.py"
+
+	## RUN Above Commands
+	echo -e "--- Executing ${CO1} ---"
+	eval "$CO1"
+	echo -e ""
+	echo -e ""
+	echo -e ""
+
+}
+
+# --- Execution ---
+panty() {
+	uv_s1 2>&1 | tee -a sc/logz/r1.sh.txt
+}
+panty
