3700881c2c56d4d92f2ec38537e6e99825b9a71e
m0x0m0x <123001669+m0x0m0x@users.noreply.github.com>
2026-04-28 14:23:33 +0000

    🤡 p3 work

--- W/pfla/p3/app.py
+++ W/pfla/p3/app.py
@@ -27,6 +27,8 @@ def index():
 
     return render_template('index.html', myResult=myResult, myValue=myValue, mylist=mylist, mylist2=mylist2)
 
+# i2 Route - tempz/i2.html - Testing base templating
+
 
 @app.route("/i2")
 def i2():
@@ -37,6 +39,19 @@ def i2():
     mylist2 = [random.randint(1, 100) for _ in range(10)]
 
     return render_template('i2.html', myResult=myResult, myValue=myValue, mylist=mylist, mylist2=mylist2)
+
+
+# f1 - Filtering route - tempz/filter.html - Testing jinja filters
+
+@app.route("/f1")
+def filter1():
+    # Variabeles for jinja templating
+    myValue = 'Booty Smeller'  # Varaible Being Called in HTML
+    myResult = 10 + 30  # Variable being called in HTML
+    mylist = [10, 20, 30, 24]
+    mylist2 = [random.randint(1, 100) for _ in range(10)]
+
+    return render_template('filter.html', myResult=myResult, myValue=myValue, mylist=mylist, mylist2=mylist2)
 
 
 if __name__ == '__main__':
