ceeb8ab720ac421b597dcf6eded661c748602d1d
m0x0m0x <123001669+m0x0m0x@users.noreply.github.com>
2026-04-29 14:25:50 +0000

    🤡⏺️  p4 work

--- W/pfla/p4/main.py
+++ W/pfla/p4/main.py
@@ -1,5 +1,14 @@
+# -----------------------------
+# V4 - Testing forms and post messages to endpoint
+# Note - This project will also be organized
+# -----------------------------
+
+# --- Imports ---
+
 def main():
     print("Hello from p4!")
+
+# --- Execution ---
 
 
 if __name__ == "__main__":
