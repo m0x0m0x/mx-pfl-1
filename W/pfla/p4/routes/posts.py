8c715ceeb001756d465919d163152678696d4dba
m0x0m0x <123001669+m0x0m0x@users.noreply.github.com>
2026-04-30 11:04:00 +0000

    🤡⏺️  p4 work

--- W/pfla/p4/routes/posts.py
+++ W/pfla/p4/routes/posts.py
@@ -1,9 +1,9 @@
 
-from flask import Blueprint
+from flask import Blueprint, render_template
 
 posts_bp = Blueprint('posts', __name__)
 
 
 @posts_bp.route('/posts')
 def posts():
-    return "Posts"
+    return render_template('posts.html')
