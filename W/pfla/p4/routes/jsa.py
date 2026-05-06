5fad687d40da7e777a5a0b25967256efac53c9d3
m0x0m0x <123001669+m0x0m0x@users.noreply.github.com>
2026-05-06 15:49:17 +0000

    🤡⏺️  p4 work

--- W/pfla/p4/routes/jsa.py
+++ W/pfla/p4/routes/jsa.py
@@ -3,7 +3,7 @@
 # -----------------------------------
 
 
-from flask import Blueprint, render_template
+from flask import Blueprint, jsonify, render_template, request
 
 jsa_bp = Blueprint('jsa', __name__)
 
@@ -15,3 +15,14 @@ def jsa1():
     return render_template('jsa.html')
 
 # JS Wil go in here
+
+
+@jsa_bp.route('/handle_post', methods=['POST'])
+def handle_post():
+    greeting = request.json['greeting']
+    name = request.json['name']
+
+    with open('file.txt', 'w') as f:
+        f.write(f'{greeting}, {name}!')
+
+    return jsonify({'message': 'Data fucked now in file.txt'})
