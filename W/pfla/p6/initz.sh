9898649abb8c94ac10a16f96fe1f6a2288c05467
m0x0m0x <123001669+m0x0m0x@users.noreply.github.com>
2026-05-16 14:48:41 +0000

    ✒️ P6 - Flask DB Ops

---
+++ W/pfla/p6/initz.sh
@@ -0,0 +1,64 @@
+#!/usr/bin/env bash
+
+# -------------------------
+#  initz.sh - Script to setup the project
+# -------------------------
+
+# Error Handling
+set -euo pipefail
+
+# Colors
+BBLACK='\033[1;90m'
+BRED='\033[1;91m'
+BGREEN='\033[1;92m'
+BYELLOW='\033[1;93m'
+BBLUE='\033[1;94m'
+BMAGENTA='\033[1;95m'
+BCYAN='\033[1;96m'
+BWHITE='\033[1;97m'
+RESET='\033[0m'
+
+# -- Commands ---
+
+hea1() {
+	echo -e ""
+	echo -e ""
+	echo -e "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
+	echo -e "$1$"
+	echo -e "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
+}
+
+# Setup UV Project and run
+uv1() {
+
+	declare -a CMD=(
+
+		"uv init ."
+		"uv run main.py"
+		"uv add flask flask-cors routes flask-limiter redis urllib3"
+		"uv add  Flask-Limiter[cli]"
+
+	)
+
+	for CMDEXEC in "${CMD[@]}"; do
+		echo -e "${BBLUE}────── ꒰ঌ·✦·໒꒱ ──────${RESET}"
+		echo -e "${BBLUE}────── UV Command Run ──────${RESET}"
+		echo -e "${BBLUE}────── ꒰ঌ·✦·໒꒱ ──────${RESET}"
+		echo -e "Executing: ${CMDEXEC}"
+		eval "${CMDEXEC}"
+		echo -e "${BGREEN}Done!${RESET}"
+		echo -e "${BBLUE}───── ⋆⋅☆⋅⋆ ─────${RESET}"
+		echo -e "${BBLUE}───── ⋆⋅☆⋅⋆ ─────${RESET}"
+		echo -e "${BBLUE}───── ⋆⋅☆⋅⋆ ─────${RESET}"
+		echo # Add blank line between commands
+		echo # Add blank line between commands
+		echo # Add blank line between commands
+	done
+}
+
+# --- Execution ---
+panty() {
+	uv1 2>&1 | tee -a ./sc/logz/initz.sh.txt
+
+}
+panty
